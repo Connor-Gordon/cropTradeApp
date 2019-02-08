2f997a13b7b4888254480c8018f6886b4458d588
Connor Gordon <con.gordon@gmail.com>
2019-02-07 19:42:16 -0800

    added search

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
 import store from '../store'
-
+import { Authentication, AuthRoute } from '../lib/auth'
 
 
 import Home from './Home'
@@ -11,7 +11,8 @@ import Post from './Post'
 import Scat from './Singlecat'
 import Form from './Form'
 import About from './About'
-
+import Search from './Search'
+import SignIn from './SignIn'
 
 
 class App extends Component {
@@ -20,13 +21,16 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div>
+            <Search />
             <Switch>
-              
+              <Route path="/signin" component={SignIn} />
               <Route exact path="/"  component={Home} />
               <Route path="/about" component={About} />
               <Route  path="/post/:id"  component={Post} />
               <Route  path="/posts/:slug/:id"  component={Scat} />
-              <Route path="/form/:slug/:id" component={Form} />
+              <Authentication redirectUrl="/signin">
+                <Route path="/form/:slug/:id" component={Form} />
+              </Authentication>
               <Route  path="/:slug/:id"  component={List} /> 
               
             </Switch>
